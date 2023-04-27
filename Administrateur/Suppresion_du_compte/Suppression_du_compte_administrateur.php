a1626bec3eda0dcf5cdbeb047b96013a990f4833
Moad <abdermoad@gmail.com>
2023-04-27 22:19:22 +0200

    css

---
+++ Administrateur/Suppresion_du_compte/Suppression_du_compte_administrateur.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../../Origine/CSS/1_Header.css">
+    <link rel="stylesheet" type="text/css" href="Suppression_du_compte_administrateur.css"> 
+    <link rel="stylesheet" type="text/css" href="../../Origine/CSS/3_Footer.css">
+    <title>Metric Care</title>
+    <link rel="icon" href="../../Origine/Images/Logo.png">
+  </head>
+  <body>
+    <header>
+      <div id="Rectangle_Debut">
+        <nav>
+          <div>
+            <img id="Logo" src="../../Origine/Images/Logo.png" alt="Logo Metric Metro", width="75", height="40">
+            <img id="Logo_Texte" src="../../Origine/Images/Logo_texte.png" alt="Logo Metric Metro", width="200", height="50">
+            <a href="https://infinitemeasures.fr/vues/fr/index.php" target="_blank">
+            <img id="Logo_Infinite_Measures" src="../../Origine/Images/Infinite_logo.png" alt="Logo Infinite Measures", width="60", height="60"> </a>
+          </div>      
+          <div class="contenuNav">
+            <a>🌐Français</a>
+            <a>Contact</a>
+            <a>FAQ</a>
+            <a>Qui sommes nous ?</a>
+            <a>Connexion</a>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+    <main>
+      <div>
+        <br><br><br><br>
+        <div class="Connexion">
+            <a>Suppression du compte</a>
+        </div>
+        <br>
+        <div class="container">
+            <input type="number" placeholder="Numéro d'identification"><br></br>
+            <input type="password" placeholder="Mot de passe"><br></br>
+            <div class="emailbox"><div class="emailboxbox"><input type="email" placeholder="Email"></div> 
+            <div class="rectangle-envoye">
+            <a>Envoyer Code par email</a>
+            </div></div>
+            <br></br>
+            <input type="number" placeholder="Code de suppression">
+        </div>
+        <div>
+          <a class = "sursupr"> Etes-vous sûres de vouloir supprimer votre compte ?</a> 
+          <br><br>
+        </div>
+        <div class="rectangle-connecter">
+            <a>Valider</a>
+        </div>
+      </div>
+    </main>
+
+    <!--
+    <footer class = "Footer">
+      <div class = "contenu_Footer">
+        <div class="Footer_contenu_Logo">
+          <img id="LogoFooter" src="../../Origine/Images/image.png" alt="Logo Metric Metro", width="80", height="80">
+          <p>Metric Care © 2023 - Tous droits réservés</p>
+          <a href="https://infinitemeasures.fr/vues/fr/index.php" target="_blank">
+          <img id="Logo_Infinite_Measures_Footer" src="../../Origine/Images/Infinite_logo.png" alt="Logo Infinite Measures", width="60", height="60"> </a>
+        </div>
+        <div class="Footer_contenu_texte">
+          <a>CGU</a> <a>|</a>
+          <a>À propos</a> <a>|</a>
+          <a>Préférences des cookies</a>
+        </div>
+      </div>
+    </footer>-->
+  </body>
+</html>

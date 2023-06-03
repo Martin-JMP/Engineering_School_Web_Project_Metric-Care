ecb5ab631ad8b4c7524c0bf5e2f24d592cfdd7c6
janeair36241257@gmail.com <janeair36241257@gmail.com>
2023-06-03 18:07:43 +0200

    English version

---
+++ view/Authentification/Connexion/ConnectionA.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="Connexion.css"> 
+    <link rel="stylesheet" type="text/css" href="../../Origine/CSS/1_Header.css">
+    <link rel="stylesheet" type="text/css" href="../../Origine/CSS/3_Footer.css">
+    <title>Metric Care</title>
+    <link rel="icon" href="../../Origine/Images/Logo.png">
+  </head>
+  <body>
+    <?php
+      function alert($msg) {
+        echo "<script type='text/javascript'>alert('$msg');</script>";
+      }
+      if (isset($_POST['button'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $bdd = new PDO("mysql:host=localhost;dbname=metric_care","vapr@metric.care","vapr333!");
+          if (!empty($email && $password)){
+            try{
+              /*$method = "aes-256-cbc";
+              $key = "secretkeyofmetriccare";
+              $options = 0;
+              $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));*/
+              $sth = $bdd->query("SELECT logins.MotDePas FROM logins, personnes  WHERE logins.PersonneId = personnes.PersonneId AND personnes.AdressMail = '$email'");
+              //$sth = implode($bdd->query("SELECT logins.MotDePas FROM logins, personnes  WHERE logins.PersonneId = personnes.PersonneId AND personnes.AdressMail = '$email'")->fetch());
+              $post = $sth->fetch();
+              //$decrypted = openssl_decrypt($sth, $method, $key, $options, $iv);
+              if($post){
+                session_start();
+                if ($email == "ad@metric.care"){
+                  header('Location: ../../Administrateur/Tableau_de_Bord_Admin/Tableau_de_Bord_Admin.PHP');
+                } else 
+                if($email == "gest1@metric.care" || $email == "gest2@metric.care" || $email == "gest3@metric.care" || $email == "gest4@metric.care"){
+                  header('Location: ../../Gestionnaire/Tableau_de_bord_Gestionnaire/Tableau_de_Bord_Gestionniare.PHP');
+                }
+                else {
+                  header('Location: ../../Utilisateur/Tableau%20de%20bord/Tableau_de_Bord.PHP');
+                }
+                $_SESSION['email'] = $email;
+                exit();
+              }else{
+                alert("Utilisateur inconnu");
+              }
+            }catch(PDOException $e){
+              $erreur = $e->getMessage();
+            }
+          }else{
+            alert('Veuillez remplir les champs obligatoires');
+          }
+        }
+    ?>
+    <header>
+      <div id="Rectangle_Debut">
+        <nav>
+          <div>
+            <img id="Logo" src="../../Origine/Images/Logo.png" alt="Logo Metric Metro", width="75", height="40">
+            <img id="Logo_Texte" src="../../Origine/Images/Logo_texte.png" alt="Logo Metric Metro", width="200", height="50">
+            <a href="https://infinitemeasures.fr/vues/fr/index.php" target="_blank">
+            <img id="Logo_Infinite_Measures" src="../../Origine/Images/Infinite_logo.png" alt="Logo Infinite Measures", width="60", height="60"> </a>
+          </div>      
+          <div class="contenuNav">
+          <a href="Connexion.PHP">🌐English</a>
+            <a href="../../General/Contact/Contact.PHP">Contact</a>
+            <a href="../../General/FAQ/FAQ.PHP">FAQ</a>
+            <a href="../../General/AboutUs/AboutUs.PHP">About us</a>
+            
+          </div>
+        </nav>
+      </div>
+    </header>
+    <main method="post" form="login-form">
+      <div class="tot">
+        <div class="Connexion">
+            <a>Connexion</a>
+        </div>
+        <form id="login-form"  method="post">
+          <div class="container">
+              <input type="email" id='email' name="email" placeholder="Email"><br></br>
+              <input type="password" id='password' name="password" placeholder="Password">
+          </div>
+          <div class="MDP_OUBLIE">
+              <a href = "../Mot_de_passe_oublie/Mot_de_passe_oublie.PHP">Forgot your password ?</a>
+          </div>
+          <div class="rectangle-connecter" name="button">
+            <button type="submit" name="button">Log in</button>
+          </div>
+        </form>
+        <div class = "Pas_compte">
+            <div>
+                <div><a>Don't have</a></div>
+                <div><a>an account ?</a></div>
+            </div>
+            <div class="Inscrivez-vous" href="#">
+                <div><a href="../Inscription/InscriptionA.php">Register</a></div>
+                <div><a href="../Inscription/InscriptionA.php">now</a></div>
+            </div>
+        </div>
+      </div>
+    </main>
+    <footer class = "Footer">
+      <div class = "contenu_Footer">
+        <div class="Footer_contenu_Logo">
+          <img id="LogoFooter" src="../../Origine/Images/image.png" alt="Logo Metric Metro", width="80", height="80">
+          <p>Metric Care © 2023 - All rights reserved</p>
+          <a href="https://infinitemeasures.fr/vues/fr/index.php" target="_blank">
+          <img id="Logo_Infinite_Measures_Footer" src="../../Origine/Images/Infinite_logo.png" alt="Logo Infinite Measures", width="60", height="60"> </a>
+        </div>
+        <div class="Footer_contenu_texte">
+          <a href="../../General/CGU/CGU.PHP">GTU</a> <a>|</a>
+          <a href="../../General/AboutUs/AboutUs.PHP">About us</a> <a>|</a>
+          <a href="../../General/Cookies/Cookies.PHP">Cookie preferences</a>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ view/Authentification/Inscription/InscriptionA.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="Inscription.css"> 
+    <link rel="stylesheet" type="text/css" href="../../Origine/CSS/1_Header.css">
+    <link rel="stylesheet" type="text/css" href="../../Origine/CSS/3_Footer.css">
+    <title>Metric Care</title>
+    <link rel="icon" href="../../Origine/Images/Logo.png">
+  </head>
+  <body>
+    <?php
+        if (isset($_POST['button'])){
+
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $password1 = $_POST['password1'];
+            $prenom = $_POST['prenom'];
+            $nom = $_POST['nom'];
+            $identification = $_POST['identification'];
+
+            $bdd = new PDO("mysql:host=localhost;dbname=metric_care","valentyna","12345");
+            $sth = $bdd->query("SELECT * FROM authentificationprimaire  WHERE authentificationprimaire.AuthentificationId = '$identification'");
+            $sthf = $sth->fetch();
+            if (!empty($email && $password && $prenom && $nom && $identification) /*&& $password == $password1 && $sthf.rowCount() > 0*/){
+                try{
+                    $ins1 = $bdd->query("INSERT INTO personnes(Prenom, Nom, Adressmail) VALUES ('$prenom','$nom','$email')");
+                    $ins2 = $bdd->query("INSERT INTO logins(PersonneId, MotDePas) VALUES((SELECT personnes.PersonneId FROM personnes where personnes.AdressMail = '$email'),'$password')");
+                    $upd = $bdd->query("UPDATE authentificationprimaire SET authentificationprimaire.PersonneId = (SELECT personnes.PersonneId FROM personnes where personnes.AdressMail = '$email') where authentificationprimaire.AuthentificationId = $identification"); 
+                    $post1 = $ins1->fetch();
+                    $post2 = $ins2->fetch();
+                    $pos3 = $upd->fetch();
+                    if($post1 && $post2 && $upd){
+                      
+                      header('Location: ../Connexion/Connexion.PHP');
+                      exit();
+              
+                    }
+                    else{
+                      echo "Le problème de la création";
+                    }
+                }catch(PDOException $e){
+                    $erreur = $e->getMessage();
+                }
+
+            }
+            else{
+                echo 'Veuillez remplir les champs obligatoires';
+            }
+        }
+    ?>
+    <header>
+      <div id="Rectangle_Debut">
+        <nav>
+          <div>
+            <img id="Logo" src="../../Origine/Images/Logo.png" alt="Logo Metric Metro", width="75", height="40">
+            <img id="Logo_Texte" src="../../Origine/Images/Logo_texte.png" alt="Logo Metric Metro", width="200", height="50">
+            <a href="https://infinitemeasures.fr/vues/fr/index.php" target="_blank">
+            <img id="Logo_Infinite_Measures" src="../../Origine/Images/Infinite_logo.png" alt="Logo Infinite Measures", width="60", height="60"> </a>
+          </div>      
+          <div class="contenuNav">
+          <a href="Connexion.PHP">🌐English</a>
+            <a href="../../General/Contact/Contact.PHP">Contact</a>
+            <a href="../../General/FAQ/FAQ.PHP">FAQ</a>
+            <a href="../../General/AboutUs/AboutUs.PHP">About us</a>
+            <a href="../Connexion/ConnectionA.php">Connection</a>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <main method="post" form="inscription-form">
+        <div class="tot">
+        <div class="Inscription">
+            <a>Inscription</a>
+        </div>
+        <form id="inscription-form"  method="post">
+            <div class="Prenom_Nom">
+                <input type="text" id="prenom" name="prenom" placeholder="Name">
+                <input type="text" id="nom" name="nom" placeholder="Surname">
+            </div>
+            <div class="container">
+                <div class="email">
+                    <input type="email" placeholder="Email" name="email">
+                </div>
+                <div class="NUM_IDENT">
+                    <input type="number" name="identification" placeholder="Identification number" pattern="[0-9]{6}" title="Le Numéro d'Identification doit comporter six chiffres">
+                </div> 
+                <div class="mdp">
+                    <input type="password" name="password" placeholder="Password">
+                </div> 
+                <div class="mdp_Confirme">
+                    <input type="password" name="password1" placeholder="Confirm the password">
+                </div>    
+            </div>
+            <br></br>
+            <div class="CGU">
+                <input type="checkbox" name="cgu" id="cgu">
+                <label for="cgu">I accept the <span><a  href="../../General/CGU/CGU.PHP">general terms of use</a></span></label>
+            </div>
+        
+            <div class="rectangle-creer">
+            <button type="submit" name="button">Create my account</button>
+            </div>
+        </div>
+        </form>
+    </div>
+    </main>
+    <footer class = "Footer">
+      <div class = "contenu_Footer">
+        <div class="Footer_contenu_Logo">
+          <img id="LogoFooter" src="../../Origine/Images/image.png" alt="Logo Metric Metro", width="80", height="80">
+          <p>Metric Care © 2023 - All rights reserved</p>
+          <a href="https://infinitemeasures.fr/vues/fr/index.php" target="_blank">
+          <img id="Logo_Infinite_Measures_Footer" src="../../Origine/Images/Infinite_logo.png" alt="Logo Infinite Measures", width="60", height="60"> </a>
+        </div>
+        <div class="Footer_contenu_texte">
+          <a href="../../General/CGU/CGU.PHP">GTU</a> <a>|</a>
+          <a href="../../General/AboutUs/AboutUs.PHP">About us</a> <a>|</a>
+          <a href="../../General/Cookies/Cookies.PHP">Cookie preferences</a>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>

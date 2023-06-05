41d7b634847a21d7af03a7ef3495fc2d78813281
janeair36241257@gmail.com <janeair36241257@gmail.com>
2023-06-05 12:13:35 +0200

    works

--- database/metric_care.sql
+++ database/metric_care.sql
@@ -61,13 +61,24 @@ INSERT INTO `personnes` (`PersonneId`, `Prenom`, `Nom`, `AdressMail`, `DateDeNai
 (3, 'Ange', 'Courtemanche', 'acourtemanche@sncf.fr', '1969-01-22', 80, 175, 'conducteur', 2),
 (4, 'Simon', 'Traverse', 'straverse@yahoo.com', NULL, NULL, NULL, NULL, NULL),
 (5, 'Valentyna', 'Pronina', 'valentyna.pronina@eleve.isep.fr', NULL, NULL, NULL, NULL, NULL),
-(6, 'Ihor', 'Pronin', 'k_sqats@ukr.net', NULL, NULL, NULL, NULL, NULL),
-(7, 'Inter', 'Ert', 'yui@google.com', NULL, NULL, NULL, NULL, NULL),
 (8, 'Adrian', 'Chalamet', 'ad@metric.care', NULL, NULL, NULL, NULL, NULL),
 (9, 'Jana', 'Romain', 'gest1@metric.care', NULL, NULL, NULL, NULL, NULL),
 (10, 'Luka', 'Wolf', 'gest2@metric.care', NULL, NULL, NULL, NULL, NULL),
 (11, 'Max', 'Fern', 'gest3@metric.care', NULL, NULL, NULL, NULL, NULL),
-(12, 'Anastasiia', 'Nuxe', 'gest4@metric.care', NULL, NULL, NULL, NULL, NULL);
+(12, 'Anastasiia', 'Nuxe', 'gest4@metric.care', NULL, NULL, NULL, NULL, NULL),
+(13, 'Valentyna', 'Pronina', 'valentyna.pronina@ukr.net', NULL, NULL, NULL, NULL, NULL),
+(14, 'Tom', 'Ho', 'htom@oui.fr', NULL, NULL, NULL, NULL, NULL),
+(15, 'Luc', 'Wain', 'lwain@exp.fr', NULL, NULL, NULL, NULL, NULL),
+(16, 'Tom', 'Ho', 'htom@oui.fr', NULL, NULL, NULL, NULL, NULL),
+(17, 'Jana', 'Pon', 'jpon@inc.com', NULL, NULL, NULL, NULL, NULL),
+(18, 'Rom', 'Romain', 'rromain@idk.com', NULL, NULL, NULL, NULL, NULL),
+(23, 'Tai', 'Lyn', 'tlyn@hoc.de', NULL, NULL, NULL, NULL, NULL),
+(24, 'Luis', 'Roux', 'lrox@un.eu', NULL, NULL, NULL, NULL, NULL),
+(25, 'Huan', 'Lopes', 'hlopes@ukr.net', NULL, NULL, NULL, NULL, NULL),
+(26, 'Ioan', 'Paul', 'pioan@email.com', NULL, NULL, NULL, NULL, NULL),
+(30, 'Jana', 'Poit', 'jpoit@uk.com', NULL, NULL, NULL, NULL, NULL),
+(31, 'Uan', 'Chin', 'uchin@ui.you', NULL, NULL, NULL, NULL, NULL),
+(32, 'Veronica', 'Lain', 'vlain@inh.com', NULL, NULL, NULL, NULL, NULL);
 
 CREATE TABLE IF NOT EXISTS `capteurs` (
   `CapteurId` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -104,18 +115,28 @@ CREATE TABLE IF NOT EXISTS `logins` (
 
 
 INSERT INTO `logins` (`LoginId`, `MotDePas`, `PersonneId`) VALUES
-(1, '!icgWdyuwvmcJbcj1', 1),
-(2, 'hhwb1hcwjhcjkn!', 2),
-(3, 'jsbnwdhj67?', 3),
-(4, 'jdnwbfbwefkjjkefw', 4),
-(5, 'qwerty', 5),
-(6, '12345', 6),
-(7, 'asdfg', 7),
-(8, 'hr*!AqLn2', 8),
-(9, 'gest80non!', 9),
-(10, 'gest81non!', 10),
-(11, 'gest82non!', 11),
-(12, 'gest83non', 12);
+(1, '61888ca691ebc5b5449b51be0f2e8357', 1),
+(2, '0497f67ebec08b3472cf878d26897c1a', 2),
+(3, '0817fd40d62beb7718d452bbe44432c2', 3),
+(4, 'd612c71c713b0dd0e8a46482403ef96a', 4),
+(5, '897c8fde25c5cc5270cda61425eed3c8', 5),
+(8, '6445c6fdb1d130059a5e274a3b25b470', 8),
+(9, 'c34e21ab92c17e816c7b57adc582dda7', 9),
+(10, 'e5be867b450d01ca935e742efe4a3757', 10),
+(11, 'ed0b086f30f09ad3e0c81e7a8b9ef265', 11),
+(12, 'e5d89ac43f1f411f7a6653b2a30d0949a', 12),
+(13, 'e10adc3949ba59abbe56e057f20f883e', 13),
+(14, '1ca1d6cd5b5e98d1eca0f117ba21ebf5', 14),
+(15, '2fdda202e6817c1edef63f8981df9ae8', 15),
+(16, 'be73550616936a637426b4d66a5fa99f', 17),
+(17, 'b97c19f6058f62f0dad69f8ee741e0bc', 18),
+(18, 'd8578edf8458ce06fbc5bb76a58c5ca4', 23),
+(19, 'dd529904c242f0ffdf07ecff058513d0', 24),
+(20, 'd8578edf8458ce06fbc5bb76a58c5ca4', 25),
+(21, '6979c66655648273a3a2c8ab8b474ff7', 26),
+(22, 'e10adc3949ba59abbe56e057f20f883e', 30),
+(23, 'd8578edf8458ce06fbc5bb76a58c5ca4', 31),
+(24, '6979c66655648273a3a2c8ab8b474ff7', 32);
 
 CREATE TABLE IF NOT EXISTS photos (
   id INT AUTO_INCREMENT NOT NULL  PRIMARY KEY,
@@ -162,20 +183,33 @@ CREATE TABLE IF NOT EXISTS `authentificationprimaire` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `authentificationprimaire` (`AuthentificationId`, `PersonneId`) VALUES
-(221209, NULL),
+(120003, NULL),
+(120004, NULL),
+(120006, NULL),
+(120007, NULL),
 (314806, NULL),
-(111111, 8),
-(222220, 9),
-(222221, 10),
-(222222, 11),
-(222223, 12),
 (874805, 1),
 (390551, 2),
 (572381, 3),
 (573832, 4),
 (402313, 5),
-(479338, 6),
-(383969, 7);
+(111111, 8),
+(222220, 9),
+(222221, 10),
+(222222, 11),
+(222223, 12),
+(123456, 13),
+(700000, 14),
+(700001, 15),
+(700002, 17),
+(700003, 18),
+(700005, 23),
+(700004, 24),
+(221209, 25),
+(120000, 26),
+(120005, 30),
+(120001, 31),
+(120002, 32);
 
 CREATE USER 'vapr@metric.care'@'localhost' IDENTIFIED BY 'vapr333!';
 CREATE USER 'abmo@metric.care'@'localhost' IDENTIFIED BY 'abmo111?';
--- view/Authentification/Connexion/hashtest.php
+++ view/Authentification/Connexion/hashtest.php
@@ -1,16 +1,5 @@
 <?php
-$data = "kERQzPgYHNt2HOZWjxyAHQ==";
-$method = "aes-256-cbc";
-$key = "secretkeyofmetriccare";
-$options = 0;
-$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
-
-// Encrypt the data
-$encrypted = openssl_encrypt($data, $method, $key, $options, $iv);
-
-// Decrypt the data
-$decrypted = openssl_decrypt($data, $method, $key, $options, $iv);
-
-echo "Encrypted: " . $decrypted  ;
+$data = "!icgWdyuwvmcJbcj1";
+echo md5($data)
 
 ?>

ed76123a48c315d6472197d8e99f57ba34a7724e
pierre <pierre.sikora@gmail.com>
2023-06-06 00:09:57 +0200

    modif Forum

--- view/Utilisateur/Forum/forum_cree_article.php
+++ view/Utilisateur/Forum/forum_cree_article.php
@@ -5,8 +5,8 @@ session_start();
 require_once('../Forum/database.php');
 
 if (isset($_POST['titre']) && isset($_POST['message'])) {
-    $id = $_SESSION['user'];
-
+    //$id = $_SESSION['user'];
+/*
     if (!empty($_SESSION['nom']) && !empty($_SESSION['prenom'])) {
         $nom = $_SESSION['nom'];
         $prenom = $_SESSION['prenom'];
@@ -14,15 +14,29 @@ if (isset($_POST['titre']) && isset($_POST['message'])) {
         header('Location: ./forum.php?error=missing_data');
         exit();
     }
+*/
+
+    if (!empty($_SESSION['email']) ) {
+        $email = $_SESSION['email'];
+        $sql = "SELECT Nom,Prenom FROM personnes WHERE AdressMail = :email";
+        $stmt = $mysqlInstance->prepare($sql);
+        $stmt->bindValue(':prenom', $prenom);
+        $stmt->bindValue(':nom', $nom);
+        $stmt->execute();
+    } else {
+        header('Location: ./sujet.php?error=missing_data');
+        exit();
+    }
+
     $date = (new DateTime('now', new DateTimeZone('Europe/Paris')))->format('Y-m-d H:i:s');
     $titre = filter_var($_POST['titre'], FILTER_SANITIZE_STRING);;
     $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-    $statement = $mysqlInstance->prepare("INSERT INTO forum (ID, nom, prenom, date, titre, message) VALUES (:id, :nom, :prenom, :date, :titre, :message)");
+    $statement = $mysqlInstance->prepare("INSERT INTO forum (Nom, Prenom, email, date, titre, message) VALUES (:nom,:prenom,:email, :date, :titre, :message)");
     $statement->execute([
-        'id' => $id,
         'nom' => $nom,
         'prenom' => $prenom,
+        'email' => $email,
         'date' => $date,
         'titre' => $titre,
         'message' => $message

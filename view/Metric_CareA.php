e79a95752397b310eef5d3a75c4f68e4688cb7d9
unicorn003 <80602034+unicorn003@users.noreply.github.com>
2023-06-23 11:10:14 +0200

    Create Metric_CareA.php

---
+++ view/Metric_CareA.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="Origine/CSS/1_Header.css">
+    <link rel="stylesheet" type="text/css" href="Origine/CSS/2_Main.css">
+    <link rel="stylesheet" type="text/css" href="Origine/CSS/3_Footer.css">
+    <title>Metric Care</title>
+    <link rel="icon" href="Origine/Images/Logo.png">
+  </head>
+  <body>
+    <header>
+      <div id="Rectangle_Debut">
+        <nav>
+          <div>
+            <img id="Logo" src="Origine/Images/Logo.png" alt="Logo Metric Metro", width="75", height="40">
+            <a href="Metric_Care.php">
+              <img id="Logo_Texte" src="Origine/Images/Logo_texte.png" alt="Logo Metric Metro", width="200", height="50">
+            </a>
+            <a href="https://infinitemeasures.fr/vues/fr/index.php" target="_blank">
+            <img id="Logo_Infinite_Measures" src="Origine/Images/Infinite_logo.png" alt="Logo Infinite Measures", width="60", height="60"> </a>
+          </div>      
+          <div class="contenuNav">
+            <a href="Metric_Care.php">🌐English</a>
+            <a href="General/Contact/ContactA.PHP">Contact us</a>
+            <a href="General/FAQ/FAQA.PHP">FAQ</a>
+            <a href="General/AboutUs/AboutUsA.PHP">About Us</a>
+            <a href="Authentification/Connexion/ConnectionA.php">Sing in</a>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <main>
+
+    <img id="MainPageImage" src="Origine/Image_main_accueil/Main_page.jpg" alt="Photo_bus_driver" width=100% height="448">
+    <div id="Rectangle_Blanc_1"></div>
+    <div id="Metric_Metro_Container">
+      <div id="Title_Metric_Metro">
+        <a>Metric Metro</a>
+      </div>
+      <div id="Texte_Pres_MetricMetro">
+        <a>Sensor for monitoring temperature, air quality, sound level and heart rate</a>
+      </div>
+    </div>
+
+    <div id="Product_Description_Container">
+      <div id="Description_Sentence_1">
+        <a>Metric Metro, the Metric Care product that will revolutionize the study of users' living conditions</a>
+      </div>
+      <div id="Separation_description"></div>
+      <div id="Description_Sentence_2">
+        <a>An all-in-one box, with 4 sensors, enabling users to assess their working conditions and therefore their quality of life.<br>
+          Our Metric Mtero product is specially designed for bus, streetcar and metro drivers.</a>
+      </div>
+    </div>
+
+    <div id="Expertise_Container">
+      <div id="Notre_Expertise">
+        <a>Our expertise</a>
+      </div>
+      <div id="Blocs">
+        <div id="bloc1">
+
+          <div id="Bloc1_Température">
+            <img id="Logo_Température" src="Origine/Image_main_accueil/temperature-sensor.png" alt="Logo Capteur Température", width="512", height="512">
+            <a>Real-time temperature and<br> humidity measurements</a>
+          </div>
+        </div>
+
+        <div id="bloc2">
+          <div id="Bloc2_Sonore">
+            <img id="Logo_Sonore" src="Origine/Image_main_accueil/mic-sensor.png" alt="Logo Capteur Sonore", width="60", height="59">
+            <a>Environmental measurements<br>sound in real time</a>
+          </div>
+        </div>
+
+        <div id="bloc3">
+          <div id="Bloc3_Cardiaque">
+            <img id="Logo_Cardiaque" src="Origine/Image_main_accueil/rythme-cardiaque-sensor.png" alt="Logo Capteur Cardiaque", width="72", height="74">
+            <a>Rhythm measurements<br>heart rate in bpm in<br> real time</a>
+          </div>
+        </div>
+
+        <div id="bloc4">
+          <div id="Bloc4_CO2">
+            <img id="Logo_CO2" src="Origine/Image_main_accueil/detector-CO2-sensor.png" alt="Logo Capteur CO2", width="54", height="53">
+            <a>Quality measurement<br>of the air in real time</a>
+          </div>
+        </div>
+      </div>
+      <div id="Separation_tram"></div>
+      <div id="Last_elements">
+      <img id="Tram" src="Origine/Image_main_accueil/tram.png" alt="Tram", width="679", height="337"><br><br>
+      <a>By working with major public transport and high-speed lines, we aim to<br>reduce the nuisances present in the working environment of subway drivers,<br>tramway, bus and train.</a>
+      </div>
+    </div>
+
+
+    </main>
+    <footer class = "Footer" >
+      <div class = "contenu_Footer">
+        <div class="Footer_contenu_Logo">
+          <img id="LogoFooter" src="Origine/Images/image.png" alt="Logo Metric Metro", width="80", height="80">
+          <p>Metric Care © 2023 - All rights reserved</p>
+          <a href="https://infinitemeasures.fr/vues/fr/index.php" target="_blank">
+          <img id="Logo_Infinite_Measures_Footer" src="Origine/Images/Infinite_logo.png" alt="Logo Infinite Measures", width="60", height="60"> </a>
+        </div>
+        <div class="Footer_contenu_texte">
+        <div class="Footer_contenu_texte">
+          <a href="General/CGU/CGUA.PHP">GCU</a> <a>|</a>
+          <a href="General/AboutUs/AboutUsA.PHP">About us</a> <a>|</a>
+          <a href="General/Cookies/Cookies.PHP">Cookies preferences</a>
+        </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
